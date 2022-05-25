8c13c125effacaa9f596c912161ae251ef894140
Iago Lorenzo Manrique <ilorenzomanrique@danielcastelao.org>
2022-05-25 14:36:00 +0200

    Creacion de la Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        //recojo la instancia unica
+            //Model miDB = Model.getInstance();
+        //utiliza un método de la instacia
+            //Connection miConn = miDB.getConn();
+    }
+}
